43cfbae39e32cff58b040d4e058c8a03da3f42ce
Julio Castellanos <julio.castellanos@natgeo.com>
2019-11-06 13:39:55 -0500

    update serverless

--- serverless/handler.js
+++ serverless/handler.js
@@ -4,7 +4,7 @@ module.exports.endpoint = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: `Hello, the current time is ${new Date().toTimeString()}.`,
+      message: `Hello, the current time is ${new Date().toTimeString()}. change on the serverless`,
     }),
   };
 
